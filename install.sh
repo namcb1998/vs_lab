70fb86b3380e4c488493160682da8c2e1746d3d5
namcb1998 <namcb1998@gmail.com>
2023-02-19 22:35:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,6 @@ sudo yum install nodejs -y
 sudo yum install -y yum-utils
 sudo yum-config-manager sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo yum check-updateyum check-update
-sudo yum install code -y
 sudo npm i -g pm2
 wget https://raw.githubusercontent.com/namcb1998/vs_lab/main/code
 chmod +x code
